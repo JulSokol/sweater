f62086526de3de28e7377f4bf64278c19f698d3b
Juliya <sokolenkoulia8@gmail.com>
2019-03-21 14:47:02 +0200

    update application.properties.

--- src/main/java/com/example/sweater/controller/MainController.java
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -26,9 +26,18 @@ public class MainController {
         return "greeting";
     }
     @GetMapping("/main")
-    public String main(Map<String, Object>model){
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model){
         Iterable<Message> messages = messageRepo.findAll();
-        model.put("messages", messages);
+
+        if ( filter!=null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        }else {
+            messages = messageRepo.findAll();
+        }
+
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
+
         return "main";
     }
 
@@ -46,19 +55,4 @@ public class MainController {
 
         return "main";
     }
-
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter, Map<String, Object>model){
-        Iterable<Message> messages;
-
-        if ( filter!=null && !filter.isEmpty()) {
-             messages = messageRepo.findByTag(filter);
-        }else {
-           messages = messageRepo.findAll();
-        }
-        model.put("messages", messages);
-
-        return "main";
-    }
-
 }
